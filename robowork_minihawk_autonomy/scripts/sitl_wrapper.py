fc0da7975e937699f8637f8ca1deb13bdb9f3db9
magicalfeyfenny <magicalfeyfenny@fenny.moe>
2026-04-20 16:51:55 -0700

    skeleton for ardupilot launcher

--- robowork_minihawk_autonomy/scripts/sitl_wrapper.py
+++ robowork_minihawk_autonomy/scripts/sitl_wrapper.py
@@ -1,14 +1,37 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 
-#libraries
+# Wrapper file used to launch the Gazebo SITL simulation
 
+# libraries
+import rospy
+import sys
 
-#helpers
+# class
+class SitlWrapper():
+    def __init__(self):
+        pass
+    
+    def run(self):
+        pass
 
-
-#main
+# main
 def main():
-    pass
+    #try initializing
+    try:
+        #connect as a rospy node named "sitl_wrapper"
+        rospy.init_node("sitl_wrapper")
+        #initialize the 
+        sitl = SitlWrapper()
+    except (ValueError, RuntimeError) as exc:
+        rospy.logerr("Failed to initialize node: %s", exc)
+        sys.exit(1)
+
+    #try running the simulation app
+    try:
+        sitl.run()
+    except (ValueError, RuntimeError) as exc:
+        rospy.logerr("Failed to launch ArduPilot SITL: %s", exc)
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
